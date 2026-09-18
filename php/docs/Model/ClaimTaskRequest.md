785afe32533128fa2e4faf9ee0a1a481eb6848fd
sauran <gg.sauran@gmail.com>
2026-09-18 09:37:53 +0500

    feat(branch): establish dedicated rust branch for NativeBPM Rust SDK

--- php/docs/Model/ClaimTaskRequest.md
+++
@@ -1,9 +0,0 @@
-# ClaimTaskRequest
-
-## Properties
-
-Name | Type | Description | Notes
------------- | ------------- | ------------- | -------------
-**assignee** | **string** |  |
-
-[[Back to Model list]](../../README.md#models) [[Back to API list]](../../README.md#endpoints) [[Back to README]](../../README.md)
--- typescript/dist/index.js
+++
@@ -1,4 +0,0 @@
-export { z, ZodError, ZodType, toJSONSchema, fromJSONSchema } from 'zod';
-export { InVariableSchema, OutVariableSchema, DMNInputSchema, DMNOutputSchema, DMNRuleSchema, NodeASTSchema, FlowASTSchema, WorkflowASTSchema, exportWorkflowOpenAPISchema, exportWorkflowJSONSchema, } from './schemas/workflow-ast.js';
-export { Workflow, WorkflowBuilder, Branch, WhenBuilder, ThenBuilder, WhenBranchBuilder, ThenBranchBuilder, Variable, Expression, V, v, serializeFormSchema, generateBPMNXML, evaluateDMNRule, } from './builder.js';
-export * from './client.js';
